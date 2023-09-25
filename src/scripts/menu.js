cc701c9d1ccb40eec8be181bb90743853fb4f51d
Sam Forderer <samsloanforderer@icloud.com>
2023-09-25 08:33:46 -0400

    almost done

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,5 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('expanded');
     document.querySelector('h3').classList.toggle('disappear');
+    document.querySelector('.middle').classList.toggle('shift-left');
 });
